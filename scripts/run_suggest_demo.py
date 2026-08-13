024a56ff37afe89c4418b4159ee8e1d11d01ad8a
Shmily <luuthinhunguyet123@gmail.com>
2026-08-13 14:40:28 +0700

    chore: add demo runner for DishRecommendationService

---
+++ scripts/run_suggest_demo.py
@@ -0,0 +1,7 @@
+from src.application.services.dish_recommendation_service import dish_recommendation_service
+
+print('Calling DishRecommendationService.suggest("happy")...')
+results = dish_recommendation_service.suggest('happy', top_k_restaurants_per_dish=2)
+print('Returned', len(results), 'dishes')
+for r in results[:3]:
+    print(r['dish_name'], 'confidence=', r.get('dish_confidence'), 'restaurants=', len(r.get('restaurants', [])))
